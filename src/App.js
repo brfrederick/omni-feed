1fc571d3bcc49a368e2f92ec282ac31f0d0864f7
Braxton Frederick <bfrederick@constantcontact.com>
2017-02-14 20:25:02 -0500

    add material-ui and themeing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import ThemeProvider from 'react-theme-provider';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1> App! </h1>
-      </div>
+      <ThemeProvider>
+        <div className="App">
+          <h1> App! </h1>
+        </div>
+      </ThemeProvider>
     );
   }
 }
--- src/App.stories.js
+++ src/App.stories.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import { storiesOf, action } from '@kadira/storybook';
+import { storiesOf, action, addDecorator } from '@kadira/storybook';
+import { muiTheme } from 'storybook-addon-material-ui';
+import RaisedButton from 'material-ui/RaisedButton';
 import App from './App';
+
+// add muiTheme
+addDecorator(muiTheme())
 
 storiesOf('App', module)
   .add('Test App', () => (
     <App onClck={action('app clicked')}></App>
+  ))
+  .add('raised mui button', () => (
+    <RaisedButton label="Default" />
   ));
