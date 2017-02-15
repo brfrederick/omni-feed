fc349c0a285d462f1df3e5c6cf00b1bd3493c0af
Braxton Frederick <bfrederick@constantcontact.com>
2017-02-14 20:42:17 -0500

    add storyboard knobs

--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@ import React, { Component } from 'react';
 import ThemeProvider from 'react-theme-provider';
 import './App.css';
 
+/* react-theme-provider
+ https://github.com/sm-react/react-theme-provider
+
+ <ThemeProvider
+    themes={[greyTheme, altTheme]}
+    themeInd={1}
+    override
+    setCSS={CSSrule}
+  >
+    <ThemedComponent />
+  </ThemeProvider>
+*/
 class App extends Component {
   render() {
     return (
--- src/App.stories.js
+++ src/App.stories.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { storiesOf, action, addDecorator } from '@kadira/storybook';
 import { muiTheme } from 'storybook-addon-material-ui';
+import { withKnobs, text } from '@kadira/storybook-addon-knobs';
 import RaisedButton from 'material-ui/RaisedButton';
 import App from './App';
 
-// add muiTheme
-addDecorator(muiTheme())
+addDecorator(muiTheme());
+addDecorator(withKnobs);
 
 storiesOf('App', module)
   .add('Test App', () => (
     <App onClck={action('app clicked')}></App>
   ))
   .add('raised mui button', () => (
-    <RaisedButton label="Default" />
+    <RaisedButton label={text('Button Label', 'A button')} />
   ));
