e01675ab0eee1a29fae622c306005e857e288ec5
Braxton Frederick <bfrederick@constantcontact.com>
2017-02-19 13:17:49 -0500

    reorg layout

--- src/views/index.js
+++ src/views/index.js
@@ -2,7 +2,7 @@ import { h } from 'preact'
 import { Router } from 'preact-router';
 
 import Home from './pages/home';
-import Layout from './layout';
+import Layout from './layout/';
 import Dashboard from './pages/dashboard';
 import Error404 from './pages/errors/404';
 
